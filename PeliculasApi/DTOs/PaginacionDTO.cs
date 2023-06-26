a5585fb23682c9deb90714d4f9754f880fcceffc
Vanessa Sanchez <vanessasanchezs1@outlook.com>
2023-06-26 08:01:45 -0400

    Implementando Paginacion

---
+++ PeliculasApi/DTOs/PaginacionDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace PeliculasApi.DTOs
+{
+    public class PaginacionDTO
+    {
+        public int Pagina { get; set; } = 1;
+        private int cantidadRegistrosPorPagina = 10;
+        private readonly int cantidadMaximaRegitrosPorPagina = 50;
+        
+        public int CantidadRegistrosPorPagina
+        {
+            get => cantidadRegistrosPorPagina;
+            set
+            {
+                cantidadRegistrosPorPagina = (value > cantidadMaximaRegitrosPorPagina) ? cantidadMaximaRegitrosPorPagina : value;
+            }
+        }
+    }
+}
